b435b75fc985f5a7572011fcfee6c28d8805a4a8
vyytx <dobeender@gmail.com>
2023-08-17 03:02:26 +0800

    add home page

---
+++ src/client/App.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+    import {Router, Route} from "svelte-routing";
+    import HomePage from "./pages/HomePage.svelte";
+
+    import 'bulma/css/bulma.css';
+    
+    export let url = "";
+</script>
+
+<Router url="{url}" basepath="/game">
+    <Route path="/">
+        <HomePage />
+    </Route>
+    <Route path="/about">
+        <!-- <AboutPage/> -->
+        <h1>absout page</h1>
+    </Route>
+</Router>
---
+++ src/client/pages/HomePage.svelte
@@ -0,0 +1,57 @@
+<section class="hero is-info is-fullheight">
+	<div class="hero-head">
+	</div>
+
+	<div class="hero-body">
+		<div class="container has-text-centered">
+			<div class="column is-6 is-offset-3">
+				<h1 class="title">
+					日研聖杯 Fate/XXX
+				</h1>
+				<h2 class="subtitle">
+					A tool for Holy Grail TRPG game, including dynamic map, an appropriate chatting system, basic flow controlling based on rule and others.
+				</h2>
+				<div class="box" id="loginBox">
+					<div class="field is-grouped">
+						<p class="control is-expanded">
+							<input class="input" type="text" placeholder="輸入你的真名">
+						</p>
+						<p class="control">
+							<a class="button is-info" href="/">
+								啟動！
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<script lang='ts'>
+	
+</script>
+
+<style lang='scss'>
+	section {
+		background: no-repeat fixed center center url("../assets/img/sevenServantsBg.png");
+		background-size: cover;
+	}
+
+	#loginBox {
+		background-color: #000000AA;
+
+		& input {
+			background-color: transparent;
+			outline: none;
+			border: 0px;
+			box-shadow: none;
+
+			color: #EEEEFF;
+
+			&::placeholder {
+				color: #DDDDDD
+			}
+		}
+	}
+</style>
