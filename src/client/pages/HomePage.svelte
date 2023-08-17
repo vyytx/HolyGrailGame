15c014a7294fa4827c73a7ac4bdf99c61c5fdada
vyytx <dobeender@gmail.com>
2023-08-18 05:03:51 +0800

    latest work for homepage

--- src/client/pages/HomePage.svelte
+++ src/client/pages/HomePage.svelte
@@ -1,3 +1,9 @@
+<script lang='ts' context='module'>
+	import FormInputBtnHori from "../components/FormInputBtnHori.svelte";
+
+	import axios from 'axios';
+</script>
+
 <section class="hero is-info is-fullheight">
 	<div class="hero-head">
 	</div>
@@ -6,52 +12,30 @@
 		<div class="container has-text-centered">
 			<div class="column is-6 is-offset-3">
 				<h1 class="title">
-					日研聖杯 Fate/XXX
+					Holy Grail Game
 				</h1>
+				<br>
 				<h2 class="subtitle">
 					A tool for Holy Grail TRPG game, including dynamic map, an appropriate chatting system, basic flow controlling based on rule and others.
 				</h2>
-				<div class="box" id="loginBox">
-					<div class="field is-grouped">
-						<p class="control is-expanded">
-							<input class="input" type="text" placeholder="輸入你的真名">
-						</p>
-						<p class="control">
-							<a class="button is-info" href="/">
-								啟動！
-							</a>
-						</p>
-					</div>
+				<div class="box">
+					<FormInputBtnHori placeholder="輸入你的真名" btnText="啟動 !" whenClick={login}/>
 				</div>
-			</div>
+			</div>	
 		</div>
 	</div>
 </section>
 
 <script lang='ts'>
-	
+	function login(shinmei: string) {
+		console.log(shinmei)
+		axios.post('/api/login', {
+			shinmei: shinmei
+		}).then(x => {
+
+		}).catch(err => console.error(err))
+	}
 </script>
 
 <style lang='scss'>
-	section {
-		background: no-repeat fixed center center url("../assets/img/sevenServantsBg.png");
-		background-size: cover;
-	}
-
-	#loginBox {
-		background-color: #000000AA;
-
-		& input {
-			background-color: transparent;
-			outline: none;
-			border: 0px;
-			box-shadow: none;
-
-			color: #EEEEFF;
-
-			&::placeholder {
-				color: #DDDDDD
-			}
-		}
-	}
 </style>
