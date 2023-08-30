dcf8dd365da3c6aa0451881cce4091efeb03856c
Xuan Chang <dobeender@gmail.com>
2023-08-30 16:59:01 +0800

    move out part of FormLogin component to login page

--- src/client/components/FormLogin.svelte
+++ src/client/components/FormLogin.svelte
@@ -18,32 +18,26 @@
     }
 </script>
 
-<section class="hero is-fullheight">
-    <div class="hero-body">
-        <div class="container has-text-centered">
-            <div class="column is-4 is-offset-4">
-                <h3 class="title">登入</h3>
-                <hr>
-                <p class="subtitle">請輸入使用者名稱與密碼以繼續</p>
-                <div class="box">
-                    <form>
-                        <FieldInputIconLeft icon={UserIcon} _value={username} placeholder="使用者名稱"></FieldInputIconLeft>
-                        <FieldInputIconLeft icon={LockIcon} _value={password} placeholder="密碼"></FieldInputIconLeft>
+<div class="column is-4 is-offset-4 has-text-centered">
+    <h3 class="title">登入</h3>
+    <hr>
+    <p class="subtitle">請輸入使用者名稱與密碼以繼續</p>
+    <div class="box">
+        <form>
+            <FieldInputIconLeft icon={UserIcon} _value={username} placeholder="使用者名稱 < 20字" type="text"></FieldInputIconLeft>
+            <FieldInputIconLeft icon={LockIcon} _value={password} placeholder="密碼 < 20字" type="password"></FieldInputIconLeft>
 
 
-                        <div class="field is-grouped mt-5">
-                            {#if hasRegisterBtn}
-                                 <div class="control is-expanded">
-                                     <button class="button is-fullwidth" on:click={registerBtnClick}>註冊</button>
-                                 </div>
-                            {/if}
-                            <div class="control is-expanded">
-                                <button class="button is-fullwidth is-info" on:click={loginBtnClick}>登入</button>
-                            </div>
-                        </div>                        
-                    </form>
+            <div class="field is-grouped mt-5">
+                {#if hasRegisterBtn}
+                     <div class="control is-expanded">
+                         <button class="button is-fullwidth" on:click={registerBtnClick}>註冊</button>
+                     </div>
+                {/if}
+                <div class="control is-expanded">
+                    <button class="button is-fullwidth is-info" on:click={loginBtnClick}>登入</button>
                 </div>
-            </div>
-        </div>
+            </div>                        
+        </form>
     </div>
-</section>
+</div>
--- src/client/pages/LoginPage.svelte
+++ src/client/pages/LoginPage.svelte
@@ -1,5 +1,32 @@
 <script lang='ts'>
+    import axios from 'axios';
+
     import FormLogin from "../components/FormLogin.svelte";
+    import { readURLQuery } from '../ts/util.ts';
+
+    const {fromURL, toURL} = readURLQuery(location.search);
+
+    function loginFunc(username: string, password: string) {
+        axios.post('/api/account/login', {username, password})
+            .then(res => {
+                const {loginToken} = res.data;
+                localStorage.setItem('loginToken', loginToken);
+                
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
+    function registerFunc(username: string, password: string) {
+
+    }
 </script>
 
-<FormLogin loginFunc={console.log} hasRegisterBtn={true}></FormLogin>
+<section class="hero is-fullheight">
+    <div class="hero-body">
+        <div class="container">
+            <FormLogin {loginFunc} {registerFunc} hasRegisterBtn/>
+        </div>
+    </div>
+</section>
