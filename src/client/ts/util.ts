7270c7d4c1fd69fd1421ac5ee2bfb0b5e37398ad
Xuan Chang <dobeender@gmail.com>
2023-08-31 13:45:08 +0800

    bug fix

--- src/client/ts/util.ts
+++ src/client/ts/util.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export function readURLQuery(query: string) : {[param: string]: string}{
     const regex = /([\w]+)=([\w-]+)/g;
-    const arr = query.match(regex).map(x => x.split('='));
+    const arr = (query.match(regex) || []).map(x => x.split('='));
     return Object.fromEntries(arr);
 }
